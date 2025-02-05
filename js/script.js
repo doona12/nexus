d71a3d524506efdb2348b469d024f0bba01788b5
doona12 <144002833+doona12@users.noreply.github.com>
2025-02-05 20:29:43 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-
+//Tap 클릭한거만 보이게 
 $(".clearfix li a").click(function () {
     $(this).parent().addClass('see').siblings().removeClass('see');
+});
+
+
+// Top button
+$(".Top").click(function () {
+    $("html,body").animate({
+        scrollTop:0
+    },1000)
 });
